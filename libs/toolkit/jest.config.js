c3894f8c41a7754d3d7985ac08dee2dcf21b12cc
mrodrigues95 <marcus.rodrigues95@gmail.com>
2021-09-11 14:28:08 -0400

    pnpm stuff

---
+++ libs/toolkit/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'toolkit',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/toolkit',
+};
---
+++ libs/toolkit/tailwind.config.js
@@ -0,0 +1,18 @@
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
+const { join } = require('path');
+
+module.exports = {
+  presets: [require('../../tailwind.config')],
+  purge: [
+    join(__dirname, './src/**/*.{js,ts,jsx,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
